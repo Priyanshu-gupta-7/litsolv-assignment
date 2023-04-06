a16293b0ec964307c20a6ab2fd52f1396dc28ba2
Priyanshu Gupta <58702310+Priyanshu-gupta-7@users.noreply.github.com>
2023-04-06 19:28:01 +0530

    Add files via upload

---
+++ src/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
+
+const ProductList = () => {
+  const { filter_products } = useFilterContext();
+
+  return <GridView products={filter_products} />;
+};
+
+export default ProductList;
